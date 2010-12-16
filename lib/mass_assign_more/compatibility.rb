e9bfb6cb3b1dc27b0e54aa6cbd9a8022f0a2d40c
Eric Chapweske <eac@zendesk.com>
2010-12-15 16:34:34 -0800

    Compatibility fixes

--- lib/mass_assign_more/compatibility.rb
+++ lib/mass_assign_more/compatibility.rb
@@ -2,14 +2,9 @@ module MassAssignMore
   module Compatibility
 
     def self.included(base)
-      base.class_eval do
-        MassAssignMore::Compatibility.mass_assignment_instance_methods.each { |instance_method| undef instance_method }
+      if defined?(ActiveRecord::Base) && (base == ActiveRecord::Base)
+        remove_mass_assignment_methods(base)
       end
-
-      base.instance_eval do
-        MassAssignMore::Compatibility.mass_assignment_class_methods.each { |class_method| undef class_method }
-      end
-
     end
 
     def remove_attributes_protected_from_mass_assignment(attributes)
@@ -18,12 +13,14 @@ module MassAssignMore
 
     module_function
 
-    def mass_assignment_instance_methods
-      [ :remove_attributes_protected_from_mass_assignment ]
-    end
+    def remove_mass_assignment_methods(klass)
+      klass.class_eval do
+        remove_method :remove_attributes_protected_from_mass_assignment
+      end
 
-    def mass_assignment_class_methods
-      [ :accessible_attributes, :protected_attributes, :attr_accessible, :attr_protected ]
+      klass.singleton_class.instance_eval do
+        remove_method :accessible_attributes, :protected_attributes, :attr_accessible, :attr_protected
+      end
     end
 
   end
